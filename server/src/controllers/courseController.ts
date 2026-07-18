54dc12ad80ed8c267b8a62ba44fc7f96a10f7187
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-19 02:44:45 +0500

    feat: get all course and get single course without purchase routes

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import catchAsyncError from "../middlewares/catchAsyncError.js";
 import CourseModel, { ICourse } from "../models/course.model.js";
 import ErrorHandler from "../utils/errorhandler.js";
+import { redis } from "../config/redis.js";
 
 export const createCourse = catchAsyncError(async (data: ICourse, res: Response, next: NextFunction) => {
     try {
@@ -101,3 +102,66 @@ export const editCourse = catchAsyncError(async (req: Request, res: Response, ne
 
     }
 })
+
+
+export const getSingleCourseWithoutPurchase = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params
+
+        if (!id || typeof id !== "string") {
+            return next(new ErrorHandler("Invalid course id", 400));
+        }
+
+        const isCacheExist = await redis.get(id)
+        if (isCacheExist) {
+            const course = JSON.parse(isCacheExist)
+            return res.status(200).json({
+                success: true,
+                course
+            });
+        }
+        const course = await CourseModel.findById(id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        if (!course) {
+            return next(new ErrorHandler("Course not found", 404));
+        }
+
+        await redis.set(id, JSON.stringify(course))
+
+
+        res.status(200).json({
+            success: true,
+            course
+        });
+    }
+    catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+
+    }
+})
+
+
+export const getAllCourseWithoutPurchase = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const isCacheExist = await redis.get("allCourses")
+        if (isCacheExist) {
+            const courses = JSON.parse(isCacheExist)
+            return res.status(200).json({
+                success: true,
+                courses
+            });
+        }
+        const courses = await CourseModel.find({}).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+        await redis.set("allCourses", JSON.stringify(courses))
+        
+        res.status(200).json({
+            success: true,
+            courses
+        });
+    }
+    catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+
+    }
+})
+
--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { uploadCourse, editCourse } from "../controllers/courseController.js";
+import { uploadCourse, editCourse, getSingleCourseWithoutPurchase, getAllCourseWithoutPurchase } from "../controllers/courseController.js";
 import { authorizeRoles, isAuthenticated } from "../middlewares/auth.js";
 
 const courseRouter = Router()
 
 courseRouter.post("/create", isAuthenticated, authorizeRoles("admin"), uploadCourse)
 courseRouter.patch("/edit/:id", isAuthenticated, authorizeRoles("admin"), editCourse)
-
+courseRouter.get("/get/all", getAllCourseWithoutPurchase)
+courseRouter.get("/get/:id", getSingleCourseWithoutPurchase)
 export default courseRouter
