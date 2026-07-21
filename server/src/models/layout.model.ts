a78af9912dee12a8ced1d7c479ab3e23f383164d
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-21 19:43:17 +0500

    feat: layout model design

---
+++ server/src/models/layout.model.ts
@@ -0,0 +1,76 @@
+import "dotenv/config";
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface FaqItem extends Document {
+    question: string;
+    answer: string
+}
+
+interface Category extends Document {
+    title: string
+}
+
+interface BannerImage extends Document {
+    public_id: string;
+    url: string;
+}
+
+
+interface Layout extends Document {
+    type: string;
+    faq: FaqItem[];
+    categories: Category[];
+    banner: {
+        image: BannerImage;
+        title: string;
+        subTitle: string;
+    }
+}
+
+const faqSchema: Schema<FaqItem> = new mongoose.Schema({
+    question: {
+        type: String,
+        // required: true
+    },
+    answer: {
+        type: String,
+        // required: true
+    }
+})
+
+
+const categorySchema: Schema<Category> = new mongoose.Schema({
+    title: {
+        type: String,
+        // required: true
+    }
+})
+
+const bannerImageSchema: Schema<BannerImage> = new mongoose.Schema({
+    public_id: {
+        type: String,
+        // required: true
+    },
+    url: {
+        type: String,
+        // required: true
+    }
+})
+
+const layoutSchema: Schema<Layout> = new mongoose.Schema({
+    type: {
+        type: String,
+    },
+    faq: [faqSchema],
+    categories: [categorySchema],
+    banner: {
+        image: bannerImageSchema,
+        title: { type: String },
+        subTitle: { type: String }
+    }
+})
+
+
+const LayoutModel: Model<Layout>= mongoose.model("Layout", layoutSchema)
+
+export default LayoutModel;
